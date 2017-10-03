d933051225261d76fa2d5966a01b5936d66c2a2c
megasanjay <sanjays344@mail.fresnostate.edu>
2017-10-03 02:06:52 -0700

    Gomoku

--- Gomoku/Gomoku.js
+++ Gomoku/Gomoku.js
@@ -1,54 +1,69 @@
 var table;
+var gridcell;
 
-function resizeTable(input)
+function resizeTable(tableSize) 
 {
- 	table = document.getElementById('gTable');
+    let row;
+    let cell;
+    
+    table = document.getElementById("gTable");
 
-	table.innerHTML = "";
-	let row;
-	let cell;
-
-	for(let i = 0; i <input; i++)
+	table.innerHTML = " ";
+	
+	for(let i = 0; i < tableSize; i++)
 	{
 		row = table.insertRow(i);
-		for (let j = 0; j < input ; j++)
+		for (let j = 0; j < tableSize ; j++)
 		{
 			cell = row.insertCell(j);
-			cell.innerHTML = '+';
+			cell.innerHTML = '<div class = "gridCell">' + i.toString() + ',' + j.toString() + '</div>';
 		}
 	}
 }
 
 function changeColor(gridColor)
-{
-	if (gridColor == 'sb')
-	{
-		table.style.backgroundColor = "skyblue";
-	}
-	if (gridColor == 'red')
-	{
-		table.style.backgroundColor = "red";
-	}
-	if (gridColor == 'green')
-	{
-		table.style.backgroundColor = "green";
-	}
+{   
+    switch (gridColor)
+    {
+        case 'skyblue':
+            table.style.backgroundColor = "skyblue";
+            break;
+        case 'red':
+            table.style.backgroundColor = "red";
+            break;
+        case 'green':
+            table.style.backgroundColor = "green";
+            break;
+        case 'noC':
+            table.style.background = "none";
+            break;
+        case 'fuchsia':
+            table.style.backgroundColor = "fuchsia";
+            break;
+        case 'silver':
+            table.style.backgroundColor = "silver";
+            break;
+    }
 }
 
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
+function colorDropDown() {
+    document.getElementById("colorDropdown").classList.toggle("show");
+}
+
+function sizeDropDown() {
+    document.getElementById("sizeDropdown").classList.toggle("show");
 }
 
 // Close the dropdown if the user clicks outside of it
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
+  if (!event.target.matches(".dropdownButt")) {
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var dropdowns = document.getElementsByClassName("clickContent");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
       var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
     }
   }
