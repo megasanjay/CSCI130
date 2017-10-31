a704d0f6089b6305720f4a97b0b8a7e7a75dbc4f
megasanjay <sanjays344@mail.fresnostate.edu>
2017-10-31 02:34:54 -0700

    Beautification

--- Lab 7/lab_week10Part2.php
+++ Lab 7/lab_week10Part2.php
@@ -1,38 +1,47 @@
 <?php
 
-function NL() 
+function NL()
 {
 echo "<br>";
 }
 
 $list = array(0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 57, 90, 144);
 
-function ArrayRand($n) {
-// create an array of size n
-$some_data=0;
-$array = array_fill(0,$n,$some_data);
+function ArrayRand($n)
+{
+	// create an array of size n
+	$some_data=0;
+	$array = array_fill(0, $n, $some_data);
 
-for ($i=1;$i<$n;$i++) {
-$array[$i]=rand(0,1000);
-}
-return $array;
-}
+	for ($i = 1; $i < $n; $i++)
+	{
+		$array[$i] = rand(0, 1000);
+	}
 
-function ArraySortedRand($n) {
-// create an array of size n
-$some_data=0;
-$array = array_fill(0,$n,$some_data);
-$array[0]=rand(0,10);
-for ($i=1;$i<$n;$i++) {
-$array[$i]=$array[$i-1]+rand(0,10);
-
-}
-return $array;
+	return $array;
 }
 
-function DisplayArray($a) {
-	for ($i=0;$i<count($a);$i++)
-		echo $a[$i] .'_';
+function ArraySortedRand($n)
+{
+	// create an array of size n
+	$some_data = 0;
+	$array = array_fill(0, $n, $some_data);
+	$array[0] = rand(0,10);
+
+	for ($i = 1; $i < $n; $i++)
+	{
+		$array[$i] = $array[$i - 1] + rand(0, 10);
+	}
+
+	return $array;
+}
+
+function DisplayArray($a)
+{
+	for ($i = 0; $i < count($a); $i++)
+	{
+		echo $a[$i] . '_';
+	}
 	NL();
 }
 
@@ -42,24 +51,32 @@ echo DisplayArray(ArraySortedRand(10));
 NL();
 
 
- 
-function iterative_binary_search($x, $list) {
+
+function iterative_binary_search($x, $list)
+{
 	$left = 0;
-	$right = count($list)-1;
-	while ($left <= $right) {
+	$right = count($list) - 1;
+	while ($left <= $right)
+	{
 		$mid = ($left + $right) >> 1;
- 
-		if ($list[$mid] == $x) {
+
+		if ($list[$mid] == $x)
+		{
 			return $mid;
-		} elseif ($list[$mid] > $x) {
+		}
+		elseif ($list[$mid] > $x)
+		{
 			$right = $mid - 1;
-		} elseif ($list[$mid] < $x) {
+		}
+		elseif ($list[$mid] < $x)
+		{
 			$left = $mid + 1;
 		}
 	}
+
 	return -1;
 }
- 
+
 $x = 57; // element to search
 echo iterative_binary_search($x, $list);
 NL();
@@ -68,54 +85,75 @@ echo iterative_binary_search($x, $list);
 NL();
 
 
-function fibonacci($n) {
-	if (( $n == 0 ) || ( $n == 1 )) {
-     return $n;
+function fibonacci($n)
+{
+	if (($n == 0) || ($n == 1))
+	{
+		return $n;
 	}
-	return fibonacci( $n-2 ) + fibonacci( $n-1 );
+	return fibonacci($n - 2) + fibonacci($n - 1);
 }
 
 echo "List of Fibonacci <br>";
-for ($i=1;$i<=20;$i++) {	
-	echo "Element ". $i . " with value ". fibonacci($i) ."<br>";
+for ($i = 1; $i <= 20; $i++)
+{
+	echo "Element " . $i . " with value " . fibonacci($i) . "<br>";
 }
 
-function isPrime($num) {
+function isPrime($num)
+{
     //1 is not prime
     if($num == 1)
-        return false;
+		{
+      return false;
+		}
+
     //2 is prime (the only even number that is prime)
     if($num == 2)
-        return true;
+		{
+      return true;
+		}
+
     //*if the number is divisible by two, then it's not prime and it's no longer needed to check other even numbers
-    if($num % 2 == 0) {
-        return false;
+    if($num % 2 == 0)
+		{
+      return false;
     }
+
     // Checks the odd numbers. If any of them is a factor, then it returns false.
     // The sqrt can be an aproximation, rounds it to the next highest integer value.
     $ceil = ceil(sqrt($num));
-    for($i = 3; $i <= $ceil; $i = $i + 2) {
+    for($i = 3; $i <= $ceil; $i = $i + 2)
+		{
         if($num % $i == 0)
-            return false;
+				{
+          return false;
+				}
     }
+
     return true;
 }
 
-function FindPrime($a) {
-	$k=0;
+function FindPrime($a)
+{
+	$k = 0;
+
 	//$out=new Array();
-	for ($i=0;$i<count($a);$i++) {
-	if (isPrime($a[$i])) {
-		    $out[$k]=$a[$i];
+	for ($i	= 0; $i < count($a); $i++)
+	{
+		if (isPrime($a[$i]))
+		{
+    	$out[$k]=$a[$i];
 			$k=$k+1;
 		}
 	}
+
 	return $out;
 }
 
-$a[0]=5;
-$a[1]=7;
-$a[2]=14;
+$a[0] = 5;
+$a[1] = 7;
+$a[2] = 14;
 echo DisplayArray(FindPrime($a));
 echo DisplayArray(FindPrime(ArraySortedRand(500)));
 
