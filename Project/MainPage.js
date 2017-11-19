9723578bc579500b113c8eb94e3b1b3a7e9da27b
msalomon90 <msalomon9@mail.fresnostate.edu>
2017-11-19 14:44:14 -0800

    Main Page shit

--- Project/MainPage.js
+++ Project/MainPage.js
@@ -8,18 +8,35 @@ function checkPrivilege()
 {
   user = sessionStorage.getItem("currentUser");
   admin = sessionStorage.getItem("admin");
-  //alert(admin);
+
   if (user == undefined)
   {
     alert("Please log into your account.");
     window.open("Login.html", "_self", false);
   }
-  if(admin){
-    //showAdminSettings();
+  if(admin == true){
+    showAdminSettings();
   }
+  let lastViewed = sessionStorage.getItem("lastPostViewed");
+  if(lastViewed == undefined){
+    sessionStorage.setItem("lastPostViewed", -1);
+  }
+  populateMainPage();
 
 }
-/* function showAdminSettings(){
+function populateMainPage(){
+  let lastViewed = sessionStorage.getItem("lastPostViewed");
+  if(lastViewed == -1){
 
+  }
 }
-*/
+function showAdminSettings(){
+
+  let deleteBtn = document.getElementById('deletePost');
+  deleteBtn.classList.remove('menuHide');
+  deleteBtn.classList.add('menuShow');
+}
+
+function editPost(){
+  alert("Butts");
+}
