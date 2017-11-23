e5776aa57bc51f59949087ab80e66d2266e96dba
msalomon90 <msalomon9@mail.fresnostate.edu>
2017-11-23 02:20:26 -0800

    Csian Girl
    Next button works!! YEAHHHHHHHH

--- Project/MainPage.js
+++ Project/MainPage.js
@@ -19,18 +19,90 @@ function checkPrivilege()
   }
   let lastViewed = sessionStorage.getItem("lastPostViewed");
   if(lastViewed == undefined){
-    sessionStorage.setItem("lastPostViewed", 10);
+    sessionStorage.setItem("lastPostViewed", -1);
   }
   populateMainPage();
 
 }
 function populateMainPage(){
-  let lastViewed = sessionStorage.getItem("lastPostViewed");
-  if(lastViewed == -1){   // request the latest post (ordered by date)
-    // make request objecct
+  var requestURL = "http://localhost:8888/getPost.php";
+  httpRequest = new XMLHttpRequest();
+  httpRequest.onreadystatechange = alertContents_loadMain;
+  httpRequest.open('POST', requestURL);
+  httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+
+  if(sessionStorage.getItem('lastPostViewed') == -1){
+    httpRequest.send('action=' + encodeURIComponent('view') + '&postID=' + encodeURIComponent(-1));
+  }
+  else{
+    httpRequest.send('action=' + encodeURIComponent('view') + '&postID=' + encodeURIComponent(sessionStorage.getItem('lastPostViewed')));
+  }
+}
+
+function fillInputFields(item, subClass){
+  sessionStorage.setItem('lastPostViewed', item['postID']);
+  let title = document.getElementById("postTitleBox");
+  let postContent = document.getElementById("postDescription");
+  let priceAmount = document.getElementById('postPrice');
+  let itemInfo1 = document.getElementById('itemInfo1');
+  let itemInfo2 = document.getElementById('itemInfo2');
+  let itemInfo3 = document.getElementById('itemInfo3');
+
+
+  let img = document.getElementById("postImage");
+  img.src = item['postImage'];
+
+  title.innerHTML = item['postTitle'];
+  postContent.innerHTML = item['postDescription'];
+  priceAmount.innerHTML = "Price: " + item['postPrice'];
+  if(item['postIssaBook']){
+    itemInfo1.innerHTML = "Book Title: " + subClass['bookTitle'];
+    itemInfo2.innerHTML = "Author: " + subClass['bookAuthor'];
+    itemInfo3.innerHTML = "Pages: " + subClass['bookPages'];
+  }
+  else{
+    itemInfo1.innerHTML = subClass['videoTitle'];
+    itemInfo2.innerHTML = subClass['videoDuration'];
+    itemInfo3.innerHTML = subClass['videoGenre'];
   }
 
 }
+
+function alertContents_loadMain(){
+  try
+  {
+    if (httpRequest.readyState === XMLHttpRequest.DONE)
+    {
+      alert(httpRequest.status);
+      if (httpRequest.status === 200)
+      {
+        var response = httpRequest.responseText;
+        if(response == "End of list"){
+          alert("End of List");
+          return;
+        }
+
+
+        let item = JSON.parse(response);
+        let subClass = JSON.parse(item['subClass']);
+
+        fillInputFields(item, subClass);
+      }
+      else
+      {
+        alert('There was a problem with the request.');
+      }
+    }
+	return 1;
+  }
+  catch(e) // Always deal with what can happen badly, client-server applications --> there is always something that can go wrong on one end of the connection
+  {
+    alert('Caught Exception: ' + e.description);
+  }
+
+
+}
+
 function showAdminSettings(){
 
   let deleteBtn = document.getElementById('deletePost');
@@ -46,4 +118,23 @@ function editPost(){
 
   window.open("NewPost.html", "_self", false);
   alert("Butts");
+}
+function navigateTo(action){
+  if(action == 'p'){
+    var requestURL = "http://localhost:8888/getPost.php";
+    httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = alertContents_loadMain;
+    httpRequest.open('POST', requestURL);
+    httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    httpRequest.send('action=' + encodeURIComponent('prev') + '&postID=' + encodeURIComponent(sessionStorage.getItem('lastPostViewed')));
+
+  }
+  else{
+    var requestURL = "http://localhost:8888/getPost.php";
+    httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = alertContents_loadMain;
+    httpRequest.open('POST', requestURL);
+    httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+    httpRequest.send('action=' + encodeURIComponent('next') + '&postID=' + encodeURIComponent(sessionStorage.getItem('lastPostViewed')));
+  }
 }
--- Project/NewPost.js
+++ Project/NewPost.js
@@ -9,17 +9,49 @@ function checkPrivilege(){
       window.open("Login.html", "_self", false);
     }
     if(sessionStorage.getItem('action') == "EP"){
-      fillInputFields();
+      var requestURL = "http://localhost:8888/getPost.php";
+      httpRequest = new XMLHttpRequest();
+      httpRequest.onreadystatechange = alert_fillInputFields;
+      httpRequest.open('POST', requestURL);
+      httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
+      alert(sessionStorage.getItem('lastPostViewed'));
+      httpRequest.send('action=' + encodeURIComponent('view') + '&postID=' + encodeURIComponent(sessionStorage.getItem('lastPostViewed')));
     }
 }
-function fillInputFields(){
-  var requestURL = "http://localhost:8888/getPost.php";
-  httpRequest = new XMLHttpRequest();
-  httpRequest.onreadystatechange = alert_fillInputFields;
-  httpRequest.open('POST', requestURL);
-  httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-  alert(sessionStorage.getItem('lastPostViewed'));
-  httpRequest.send('action=' + encodeURIComponent('view') + '&postID=' + encodeURIComponent(sessionStorage.getItem('lastPostViewed')));
+function fillInputFields(item, subClass){
+  let title = document.getElementById("titleTextBox");
+  let postContent = document.getElementById("descriptionTextBox");
+  let imageLink = document.getElementById("imageTextBox");
+  let priceAmount = document.getElementById('priceTextBox');
+  let bookRadio = document.getElementById('r12');
+  let videoRadio = document.getElementById('r22');
+  let bookTitle = document.getElementById('bookTitleTextBox');
+  let bookAuthor = document.getElementById('bookAuthorTextBox');
+  let bookPages = document.getElementById('bookPagesTextBox');
+  let videoTitle = document.getElementById('VideoTitleTextBox');
+  let videoDuration = document.getElementById('VideoTitleTextBox');
+  let videoGenre = document.getElementById('videoGenreDropdown');
+
+  title.value = item['postTitle'];
+  postContent.value = item['postDescription'];
+  imageLink.value = item['postImage'];
+  priceAmount.value = item['postPrice'];
+  if(item['postIssaBook']){
+    bookRadio.checked = true;
+    videoRadio.disabled = true;
+    bookTitle.value = subClass['bookTitle'];
+    bookAuthor.value = subClass['bookAuthor'];
+    bookPages.value = subClass['bookPages'];
+  }
+  else{
+    bookRadio.disabled = true;
+    videoRadio.checked = true;
+    showform('radio2');
+    videoTitle.value = subClass['videoTitle'];
+    videoDuration.value = subClass['videoDuration'];
+    videoGenre.value = subClass['videoGenre'];
+  }
+
 }
 
 function alert_fillInputFields(){
@@ -31,11 +63,9 @@ function alert_fillInputFields(){
       if (httpRequest.status === 200)
       {
         var response = httpRequest.responseText;
-        alert(response);
-        return;
         let item = JSON.parse(response);
-        alert(item['postTitle']);
-        return;
+        let subClass = JSON.parse(item['subClass']);
+        fillInputFields(item, subClass);
       }
       else
       {
@@ -66,24 +96,16 @@ function showform(val)
        videoform.style.display = 'block';
   }
 }
-
-function submit()
-{
+function validateInputInfo(){
   let title = document.getElementById("titleTextBox");
   let postContent = document.getElementById("descriptionTextBox");
   let imageLink = document.getElementById("imageTextBox");
   let priceAmount = document.getElementById('priceTextBox');
   let bookRadio = document.getElementById('r12');
   let videoRadio = document.getElementById('r22');
-  let bookTitle = document.getElementById('bookTitleTextBox');
-  let bookAuthor = document.getElementById('bookAuthorTextBox');
-  let bookPages = document.getElementById('bookPagesTextBox');
-  let videoTitle = document.getElementById('VideoTitleTextBox');
-  let videoDuration = document.getElementById('VideoTitleTextBox');
-  let videoGenre = document.getElementById('videoGenreDropdown');
 
   let errorFlag = false;
-
+  // info validation
   if (title.value == '' | priceAmount.value == '')
   {
       alert("Please fill all the required fields before submitting the post")
@@ -102,7 +124,7 @@ function submit()
       priceAmount.classList.remove("regularTextarea");
       priceAmount.classList.add("errorTextarea");
     }
-    return;
+    return false;
   }
 
   let string = imageLink.value;
@@ -112,7 +134,7 @@ function submit()
     alert("Invalid image link.")
     imageLink.classList.remove("regularTextarea");
     imageLink.classList.add("errorTextarea");
-    return;
+    return false;
   }
 
   let username = sessionStorage.getItem("currentUser");
@@ -138,105 +160,130 @@ function submit()
     alert("The price contains invalid characters");
     priceAmount.classList.remove("regularTextarea");
     priceAmount.classList.add("errorTextarea");
-    return;
+    return false;
   }
+  if(bookRadio.checked){
+    return validateBookInfo(image, price);
+  }
+  else{
+    return validateVideoInfo(image, price);
+  }
+}
+function validateBookInfo(image, price){
+  let title = document.getElementById("titleTextBox");
+  let postContent = document.getElementById("descriptionTextBox");
+  let bookTitle = document.getElementById('bookTitleTextBox');
+  let bookAuthor = document.getElementById('bookAuthorTextBox');
+  let bookPages = document.getElementById('bookPagesTextBox');
 
   errorFlag = false;
 
-  if (bookRadio.checked)
+
+  if (bookTitle.value == '' || bookAuthor.value == '' || bookPages.value == '')
   {
-    if (bookTitle.value == '' || bookAuthor.value == '' || bookPages.value == '')
+    errorFlag = true;
+  }
+
+  if (errorFlag == true)
+  {
+
+    alert("Please fill all the required fields before submitting the post");
+    if (bookTitle.value == '')
     {
-      errorFlag = true;
+      bookTitle.classList.remove("regularTextarea");
+      bookTitle.classList.remove("errorTextarea");
     }
-
-    if (errorFlag == true)
+    if (bookAuthor.value == '')
     {
-
-      alert("Please fill all the required fields before submitting the post");
-      if (bookTitle.value == '')
-      {
-        bookTitle.classList.remove("regularTextarea");
-        bookTitle.classList.remove("errorTextarea");
-      }
-      if (bookAuthor.value == '')
-      {
-        bookAuthor.classList.remove("regularTextarea");
-        bookAuthor.classList.remove("errorTextarea");
-      }
-      if (bookPages.value == '')
-      {
-        bookPages.classList.remove("regularTextarea");
-        bookPages.classList.remove("errorTextarea");
-      }
-      return;
+      bookAuthor.classList.remove("regularTextarea");
+      bookAuthor.classList.remove("errorTextarea");
     }
-
-    let pages = bookPages.value;
-
-    if(isNaN(pages) == false)
+    if (bookPages.value == '')
     {
-      pages = parseInt(pages);
-    }
-    else
-    {
-      alert("The number of pages contains invalid characters");
       bookPages.classList.remove("regularTextarea");
-      bookPages.classList.add("errorTextarea");
-      return;
+      bookPages.classList.remove("errorTextarea");
     }
+    return false;
+  }
 
-    var bookObject = new Object();
-    bookObject.username = username;
-    bookObject.postTitle = title.value;
-    bookObject.content = postContent.value;
-    bookObject.image = image;
-    bookObject.price = price;
-    bookObject.bookTitle = bookTitle.value;
-    bookObject.bookAuthor = bookAuthor.value;
-    bookObject.bookPages = pages;
-    bookObject.isbook = true;
+  let pages = bookPages.value;
 
-    submitPost(bookObject);
+  if(isNaN(pages) == false)
+  {
+    pages = parseInt(pages);
   }
   else
   {
-    if (videoTitle.value == '' || videoDuration.value == '')
-    {
-      errorFlag = true;
-    }
-
-    if (errorFlag == true)
-    {
-      alert("Please fill all the required fields before submitting the post");
-      if (videoTitle.value == '')
-      {
-        videoTitle.classList.remove("regularTextarea");
-        videoTitle.classList.remove("errorTextarea");
-      }
-      if (videoDuration.value == '')
-      {
-        videoDuration.classList.remove("regularTextbox");
-        videoDuration.classList.add("errorTextbox");
-      }
-      return;
-    }
-
-    //check duration here
-
-    var videoObject = new Object();
-    videoObject.username = username;
-    videoObject.postTitle = title.value;
-    videoObject.content = postContent.value;
-    videoObject.image = image;
-    videoObject.price = price;
-    videoObject.videoTitle = videoTitle.value;
-    videoObject.videoDuration = videoDuration.value;
-    videoObject.videoGenre = videoGenre.value;
-    videoObject.isbook = false;
-
-    submitPost(videoObject);
+    alert("The number of pages contains invalid characters");
+    bookPages.classList.remove("regularTextarea");
+    bookPages.classList.add("errorTextarea");
+    return false;
   }
+
+  var bookObject = new Object();
+  bookObject.username = sessionStorage.getItem("currentUser");
+  bookObject.postTitle = title.value;
+  bookObject.content = postContent.value;
+  bookObject.image = image;
+  bookObject.price = price;
+  bookObject.bookTitle = bookTitle.value;
+  bookObject.bookAuthor = bookAuthor.value;
+  bookObject.bookPages = pages;
+  bookObject.isbook = 1;
+
+  submitPost(bookObject);
+  return true;
+
+}
+function validateVideoInfo(image, price){
+  let title = document.getElementById("titleTextBox");
+  let postContent = document.getElementById("descriptionTextBox");
+  let videoTitle = document.getElementById('VideoTitleTextBox');
+  let videoDuration = document.getElementById('VideoTitleTextBox');
+  let videoGenre = document.getElementById('videoGenreDropdown');
+
+  let errorFlag;
+
+  if (videoTitle.value == '' || videoDuration.value == '')
+  {
+    errorFlag = true;
+  }
+
+  if (errorFlag == true)
+  {
+    alert("Please fill all the required fields before submitting the post");
+    if (videoTitle.value == '')
+    {
+      videoTitle.classList.remove("regularTextarea");
+      videoTitle.classList.remove("errorTextarea");
+    }
+    if (videoDuration.value == '')
+    {
+      videoDuration.classList.remove("regularTextbox");
+      videoDuration.classList.add("errorTextbox");
+    }
+    return false;
+  }
+
+  //check duration here
+
+  var videoObject = new Object();
+  videoObject.username = sessionStorage.getItem("currentUser");
+  videoObject.postTitle = title.value;
+  videoObject.content = postContent.value;
+  videoObject.image = image;
+  videoObject.price = price;
+  videoObject.videoTitle = videoTitle.value;
+  videoObject.videoDuration = videoDuration.value;
+  videoObject.videoGenre = videoGenre.value;
+  videoObject.isbook = 0;
+  submitPost(videoObject);
+  return true;
+}
+function submit()
+{
+  validateInputInfo();
+
 }
 
 function validateImageLink(string)
@@ -260,7 +307,13 @@ function submitPost(newObject)
   httpRequest.onreadystatechange = alertContents_submitPost;
   httpRequest.open('POST', requestURL);
   httpRequest.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
-  httpRequest.send(JSON.stringify(newObject))
+
+  if(sessionStorage.getItem('action') == 'NP'){
+    httpRequest.send('action=' + encodeURIComponent('new') + '&object=' + JSON.stringify(newObject));
+  }
+  else{
+    httpRequest.send('action=' + encodeURIComponent('edit') + '&postID=' + encodeURIComponent(sessionStorage.getItem('lastPostViewed')) + '&object=' + JSON.stringify(newObject));
+  }
 }
 
 function alertContents_submitPost()
