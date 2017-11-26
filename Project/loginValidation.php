3bc9f8f6426dd76f1d86973bba3d12c19fedf8e1
msalomon90 <msalomon9@mail.fresnostate.edu>
2017-11-25 17:41:48 -0800

    PHP files incoming!!!!!!!
    all php files for project

---
+++ Project/loginValidation.php
@@ -0,0 +1,49 @@
+<?php
+
+$servername = 'localhost'; // default server name
+$username = 'sanjay'; // user name that you created
+$password = '0cKfzCiL7yVa2iol'; // password that you created
+$dbname = 'project';
+
+if (!empty($_POST))
+{
+  $userName = $_POST['username'];
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname);
+
+  // Check connection
+  if ($conn->connect_error)
+  {
+      die("Connection failed: " . $conn->connect_error ."<br>");
+  }
+
+  $sql = "SELECT * FROM Users WHERE username = '"  . $userName . "'";
+
+  $result = $conn->query($sql);
+
+  if ($result->num_rows != 0)
+  {
+    $row = $result->fetch_assoc(); // Fetch a result row as an associative array
+    //echo $row["password"];
+    echo json_encode($row, JSON_PRETTY_PRINT);
+    return;
+  }
+  else
+  {
+    echo "Username does not exist";
+      //echo "Error 1";
+  }
+
+  $conn->close();
+
+	//echo 'Index value='. $index .'<br>';
+}
+else
+{
+  //$index = -1;
+  echo "Error 2";
+  //echo "no data supplied";
+}
+
+?>
